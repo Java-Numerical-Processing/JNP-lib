347481d734d34485aa2fc4c5222929ebd5627535
Evan Stella <evanstella>
2021-07-21 10:56:13 -0400

    implemented plot zooming and panning. Needs some work

--- src/com/evanstella/jnp/graphics/LinePlot.java
+++ src/com/evanstella/jnp/graphics/LinePlot.java
@@ -5,29 +5,39 @@ import com.evanstella.jnp.core.Numeric;
 
 import javax.swing.JComponent;
 import java.awt.*;
+import java.awt.event.*;
 
 public class LinePlot extends JComponent {
 
     private Numeric XData;
     private Numeric YData;
 
-    private int scale;
+    private double scaleFactor;
+    private double scale;
+    private int originX;
+    private int originY;
 
-    public LinePlot ( Numeric XData, Numeric YData ) {
+    public LinePlot ( Numeric XData, Numeric YData, int h, int w ) {
         super();
         this.XData = XData;
         this.YData = YData;
+        originX = h/2;
+        originY = w/2;
+        scale = 10;
+        scaleFactor = 1.1;
+
+        MouseHandler mouseHandler = new MouseHandler( this );
     }
 
     public void paintComponent (Graphics g) {
         update( g );
     }
 
+
     public void update ( Graphics g ) {
         //w is x, and h is y (as in x/y values in a graph)
-        int w = this.getWidth()/2;
-        int h = this.getHeight()/2;
-
+        int w = originX;
+        int h = originY;
 
         Graphics2D g1 = (Graphics2D) g;
         g1.setRenderingHint(
@@ -35,17 +45,18 @@ public class LinePlot extends JComponent {
                 RenderingHints.VALUE_ANTIALIAS_ON);
         g1.setStroke(new BasicStroke(2));
         g1.setColor(Color.black);
-        g1.drawLine(0,h,w*2,h);
-        g1.drawLine(w,0,w,h*2);
+        g1.drawLine(0,h,getWidth(),h);
+        g1.drawLine(w,0,w,getHeight());
         g1.drawString("0", w - 7, h + 13);
 
-
         Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke((float)1.5));
+        g2.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setStroke(new BasicStroke((float)2));
         g2.setColor(Color.red);
         //line1
         Polygon p = new Polygon();
-        scale = 20;
         double[] XPoints = XData.getDataReal();
         double[] YPoints = YData.getDataReal();
 
@@ -79,6 +90,36 @@ public class LinePlot extends JComponent {
 
     public Numeric getYData ( ) {
         return YData;
+    }
+
+
+    private class MouseHandler implements MouseMotionListener, MouseWheelListener {
+
+        public MouseHandler ( JComponent plot ) {
+            plot.addMouseMotionListener(this);
+            plot.addMouseWheelListener(this);
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            originX = e.getX();
+            originY = e.getY();
+            repaint();
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {}
+
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            int notches = e.getWheelRotation();
+            if ( notches < 0 ) scale /= scaleFactor;
+            else scale *= scaleFactor;
+
+            if (scale < 1) scale = 1;
+            if (scale > 10000) scale = 10000;
+            repaint();
+        }
     }
 
 }
